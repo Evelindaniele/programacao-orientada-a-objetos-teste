34bea26e4f9661ce2fc599f3e868f545ffa53e5e
Evelin Daniele <evelindaniele716@gmail.com>
2019-05-23 16:43:12 -0300

    adivinhacao

---
+++ jogos/adivinhacao.py
@@ -0,0 +1,21 @@
+print("**BEM VINDO AO JOGO**")
+numero_secreto = 25
+
+chute = input("digite seu chute..")
+chute_inteiro=int(chute)
+
+if(numero_secreto==chute_inteiro):
+
+  print("voce acertou")
+
+elif:(chute_inteiro<numero_secreto):
+
+  print("Chute abaixo do numero secreto")
+
+elif:(chute_inteiro>numero_secreto):
+
+  print("Chute acima do numero secreto")
+
+else 
+	
+	print(" Voce errou")
